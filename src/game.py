6b78bda234d21eac244e8797bc78e5ee7e4e4b28
Natalie Johansson <mnrjohansson@gmail.com>
2021-07-27 12:07:54 +0300

    probably working minmax, some bugs fixed

--- src/game.py
+++ src/game.py
@@ -45,14 +45,21 @@ class Board:
 
     def ai_move(self):
         # WIP
+        """
         for pos in range(self.n*self.n):
             if self.board[pos] == None:
                 self.board[pos] = "O"
                 return
 
+        """
+        move = self.ai_best_move()
+        self.board[move] = "O"
+        print("AI moves to",move)
+
     def end(self):
         if self.check_winner(self.x, self.board) != None:
             print("WINNER:",self.check_winner(self.x, self.board))
+            print(self.board, self.wins)
             exit()
         if self.check_tie(self.board):
             print("GAME ENDED IN TIE")
@@ -78,9 +85,10 @@ class Board:
         wins.append(self.check_horizontal(x, board))
         wins.append(self.check_diagonal_rd(x, board))
         wins.append(self.check_diagonal_ld(x, board))
-
+        
         for result in wins:
             if result != None:
+                self.wins = wins
                 return result
         return None
 
@@ -107,7 +115,8 @@ class Board:
         return None
 
     def check_diagonal_rd(self,x,board):
-        for i in range(0,self.n-x+1):
+        # check for bugs
+        for i in range(self.n+1,self.n-x+1):
             count = 1
             for j in range(0,self.n*self.n-i,self.n+1):
                 #self.board[i+j] = "a"
@@ -119,6 +128,7 @@ class Board:
                             return self.board[i+j]
                 if (i+j+1) % self.n == 0:
                     break
+        #check for bugs
         for i in range(self.n,self.n*(self.n-x)+1,self.n):
             count = 1
             for j in range(0,self.n*self.n-i,self.n+1):
@@ -134,7 +144,7 @@ class Board:
     def check_diagonal_ld(self,x, board):
         for i in range(x-1,self.n):
             count = 1
-            for j in range(0,self.n*self.n-i,self.n-1):
+            for j in range(self.n-1,self.n*self.n-i,self.n-1):
                 if board[i+j] != None:
                     if board[i+j] == board[i+j-(self.n-1)]:
                         count += 1
@@ -142,7 +152,7 @@ class Board:
                             return board[i+j]
                 if (i+j) % self.n == 0:
                     break
-        
+        ### check for bugs
         for i in range(2*self.n-1,self.n*(self.n-x+1),self.n):
             count = 1
             for j in range(0,self.n*self.n-i,self.n-1):
@@ -153,36 +163,43 @@ class Board:
                             return board[i+j]
         
         return None
+    
+    def ai_best_move(self):
+        best_value = -100
+        best_move = -1
+        for i in range(self.n*self.n):
+            if self.board[i] == None:
+                new_board = self.board.copy()
+                new_board[i] = "O"
+                new_value = self.minmax(new_board,self.n,False)
+                if new_value > best_value:
+                    best_value = new_value
+                    best_move = i
+        return best_move
 
-    def minmax(self,board,n,ai_turn,moves):
-        if board.check_winner() == "O":
+    def minmax(self,board,n,ai_turn):
+        if self.check_winner(self.x,board) == "O":
             return 10
-        if board.check_winner() == "X":
+        if self.check_winner(self.x,board) == "X":
             return -10
-        if board.check_tie():
+        if self.check_tie(board):
             return 0
-        print(moves)
+
         if ai_turn:
             value = -100
-            new_board = board.copy()
+            
             for i in range(n*n):
+                new_board = board.copy()
                 if new_board[i] == None:
-                    new_board[i] = "X"
-                comp = minmax(new_board, n, False,moves)
-                value = max(value, comp)
-                if value == comp:
-
-                    moves.append(i)
+                    new_board[i] = "O"
+                    value = max(value,self.minmax(new_board, n, False))
             return value
         else:
             value = 100
-            new_board = board.copy()
             for i in range(n*n):
+                new_board = board.copy()
                 if new_board[i] == None:
-                    new_board[i] = "O"
-                comp = minmax(new_board, n, True,moves)
-                value = min(value, comp)
-                if value == comp:
-                    moves.append(i)
+                    new_board[i] = "X"
+                    value = min(value,self.minmax(new_board, n,True))
             return value
             
--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 import game
 
+##debug stuff, TEMPORARY:
+board = game.Board(3)
+"""
+x1 = [None for _ in range(9)]
+x1[4] = "X"
+x1[0] = "O"
+print(x1)
+x2 = ["O","X","O","O","O","X","X",None,"X"]
+x3 = [None,"O","X","O","X","X","O","X","O"]
+x4 = ["O","O","X","X","X","O",None,None,"X"]
+#print(board.minmax(x1,3,True,""))
+y = x4
 
-board = game.Board(30)
+print(board.check_verticals(3,y))
+print(board.check_horizontal(3,y))
+print(board.check_diagonal_rd(3,y))
+print(board.check_diagonal_ld(3,y))
+print(board.check_winner(3,x4))
+"""
 
-#board.minmax(board.board,30,True,[])
 board.start_game()
