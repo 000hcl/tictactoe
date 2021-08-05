a8ed362ad8dc1ddea1e529997a0c8778bee93342
Natalie Johansson <mnrjohansson@gmail.com>
2021-08-05 17:23:36 +0300

    error in file, backup created

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from board import Board
+from backup import Board
 
-board = Board(4)
+board = Board(3)
 
 #TEMPORARY:
 
@@ -17,16 +17,18 @@ x5 = ["O","O",None,"X","O",None,"X","X","O"]
 
 y1 = [0,0,0,1,1,1,0,0,0]
 y2 = [10,0,0,10,1,1,10,1,1]
-y3 = [0 for _ in range(9)]
+y3 = [0 for _ in range(16)]
+y4 = [10,1,1,0,10,0,1,1,10]
+y5 = [1, 10, 10, 10, 1, 10, 1, 1, 1]
 
 #print(board.minmax(x1,3,True,""))
-y = y3
+y = y5
 
 #print(board.check_verticals(3,y))
 #print(board.check_horizontal(3,y))
 #print(board.check_diagonal_rd(3,y))
 #print(board.check_diagonal_ld(3,y))
-#print(board.check_winner(3,x4))
+print(board.check_winner(3,y))
 
 
 
@@ -37,4 +39,5 @@ y = y3
 #print(board.check_diagonal_ld(3,board.board))
 
 #board.print_board()
+
 board.start_game()
