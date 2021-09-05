664b01f015b06fad688b40f3fb15185a532722f2
Natalie Johansson <mnrjohansson@gmail.com>
2021-09-05 19:13:23 +0300

    update board_test

--- src/tests/board_test.py
+++ src/tests/board_test.py
@@ -1,20 +1,20 @@
 import unittest
-import src.game.board as brd
+from src.game.board import Board
 
 
 class TestBoard(unittest.TestCase):
     def setUp(self):
-        self.board = brd.Board(3)
-        self.big_board = brd.Board(30)
+        self.board = Board(3)
+        self.big_board = Board(30)
 
     def test_board_is_correct_size(self):
-        self.assertEqual(9, len(self.brd.board))
+        self.assertEqual(9, len(self.board.board))
 
     def test_check_tie_returns_true_when_board_is_full(self):
         board = ["X", "X", "O",
                  "O", "O", "X",
                  "X", "X", "O"]
 
-        result = self.brd.check_tie(board)
+        result = self.board.check_tie(board)
         self.assertTrue(result)
 
